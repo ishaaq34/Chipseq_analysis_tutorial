50c3f94dde4d8db06bcaf14ba1ae182d1c94f9ad
ishaaq34 <ishaaq34@users.noreply.github.com>
2025-12-25 07:31:02 +0000

    deploy: af8701e03cefaa021bcb4b1f08b99491249c558d

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a9e80855.js',
+                path_to_root + 'searchindex-5269c116.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
